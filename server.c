eabefc8e488940ae1099cee322b555206529eb26
Mathis Cavalli <mathiscavalli@gmail.com>
2026-03-04 15:48:42 +0100

    Cleaned up code & fixed a logic bug

--- server.c
+++ server.c
@@ -35,24 +35,23 @@ bool str_equal(const char*, const char*);
 int get_address_family(const char*);
 
 int main(int argc, char *argv[]){
-	
+
 	unsigned int verbosity = VERBOSITY_WARN;
 	unsigned short listen_ip_argv_pos = 0, listen_port_argv_pos = 0;
-	
+
 	unsigned short listen_port = 0;
 	union sockaddr_in_u sock_server, sock_client;
 	union ip_addr_u listen_addr = {0};
 	int listen_ip_family = AF_INET;
-	
-	
+
 	for (int i = 1; i < argc; i++){
-		
+
 		if (str_equal(argv[i], "-h") || str_equal(argv[i], "--help")){
-			
+
 			printf("\n    [USAGE]\n"
 				   "  [--listen, -l <ip>]\n"
 				   "  [--listen-port, -lp <port>]\n\n");
-			
+
 			return 0;
 		}
 		else if (str_equal(argv[i], "-v") || str_equal(argv[i], "--verbosity")){
@@ -61,7 +60,7 @@ int main(int argc, char *argv[]){
 				puts(MSG_ERROR "Missing number.");
 				return 1;
 			}
-			
+
 			if (str_equal(argv[i], "error"))
 				verbosity = VERBOSITY_ERROR;
 			else if (str_equal(argv[i], "warn") || str_equal(argv[i], "warning"))
@@ -72,7 +71,7 @@ int main(int argc, char *argv[]){
 				verbosity = VERBOSITY_DEBUG;
 			else
 				verbosity = atoi(argv[i]);
-			
+
 		}
 		else if (str_equal(argv[i], "-l") || str_equal(argv[i], "--listen")){
 			i++;
@@ -86,10 +85,10 @@ int main(int argc, char *argv[]){
 				printf(MSG_ERROR "Bad IP address '%s'\n", argv[i]);
 				return 1;
 			}
-			
+
 			listen_ip_argv_pos = i;
 		}
-		else if (str_equal(argv[i], "-lp") || str_equal(argv[i], "--listen-port")){
+		else if (str_equal(argv[i], "-p") || str_equal(argv[i], "--listen-port")){
 			i++;
 			if (argc == i){
 				puts(MSG_ERROR "Missing port.");
@@ -100,17 +99,17 @@ int main(int argc, char *argv[]){
 				printf(MSG_ERROR "Bad port '%s'.\n", argv[i]);
 				return 1;
 			}
-			
+
 			listen_port_argv_pos = i;
 		}
 		else{
 			printf(MSG_ERROR "Invalid argument: '%s'\n", argv[i]);
 			return 1;
 		}
-		
+
 	}
-	
-	
+
+
 	if (listen_port == 0){
 		puts(MSG_ERROR "Missing argument:\n\t--listen-port\n");
 		return 1;
@@ -129,31 +128,30 @@ int main(int argc, char *argv[]){
 		puts(MSG_ERROR "Unsupported IP family.");
 		return 1;
 	}
-	
-	
+
 	int server_socket;
 	unsigned int sizeof_sockaddr = sizeof(struct sockaddr);
-	
+
 	server_socket = socket(listen_ip_family, SOCK_DGRAM, 0);
 	if (server_socket == -1) {
 		perror(MSG_ERROR "socket()");
 		return 1;
 	}
-	
+
 
 	int option = 1;
 	if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof option) == -1){
 		perror(MSG_ERROR "setsockopt(SO_REUSEADDR)");
 		return 1;
 	}
-	
+
 	if (bind(server_socket, (struct sockaddr*)&sock_server, sizeof sock_server) != 0) {
 		perror(MSG_ERROR "bind()");
 		return 1;
 	}
-	
+
 	printf(MSG_INFO "Waiting for packets on port %d\n", listen_port);
-	
+
 	char packet[512];
 	char *packet_reply = 0;
 	int reply_size = 0;
@@ -178,16 +176,21 @@ int main(int argc, char *argv[]){
 
 			continue;		// Drop packet if under 500 bytes
 		}
-		
+
+		if (verbosity >= VERBOSITY_INFO){
+			inet_ntop(listen_ip_family, listen_ip_family == AF_INET ? &sock_client.ipv4.sin_addr.s_addr : (in_addr_t*)&sock_client.ipv6.sin6_addr, packet + 100, (sizeof packet) - 100);
+			printf(MSG_INFO "Packet from %s\n", packet + 100);
+		}
+
 		if (verbosity >= VERBOSITY_DEBUG){
 			printf("Packet dump (%db):  ", packet_size);
-			
 			for (unsigned short i = 0; i < packet_size; i++){
 				printf("%02X ", packet[i]);
 			}
 			puts("\n");
 		}
-		
+
+
 		// If the first byte is 0x00, reply with the remote IP as a 4-bytes binary representation
 		if (packet[0] == 0){
 			if (listen_ip_family == AF_INET){
@@ -198,6 +201,9 @@ int main(int argc, char *argv[]){
 		else if (packet[0] == 1){
 			// We use packet as a buffer to store the new string representation
 			inet_ntop(listen_ip_family, listen_ip_family == AF_INET ? &sock_client.ipv4.sin_addr.s_addr : (in_addr_t*)&sock_client.ipv6.sin6_addr, packet, sizeof packet);
+
+			packet_reply = packet;
+			reply_size = strlen(packet);
 		}
 		else{
 			if (verbosity >= VERBOSITY_DEBUG){
@@ -208,20 +214,12 @@ int main(int argc, char *argv[]){
 			continue;
 		}
 
-		if (verbosity >= VERBOSITY_INFO)
-			printf(MSG_INFO "Packet from %s\n", packet);
-		
-		// We point the reply pointer to the buffer containing the IP
-		packet_reply = packet;
-		reply_size = strlen(packet);
-		
-		
+
 		packet_size = sendto(server_socket, packet_reply, reply_size, 0, (struct sockaddr*)&sock_client, sizeof sock_client);
 		if (packet_size == -1){
 			perror(MSG_ERROR "Failed to reply to client request");
 			continue;
 		}
-		
 	}
 }
 
